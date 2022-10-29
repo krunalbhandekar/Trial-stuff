a574fd1c66c6a5b2be9ef47a5c8cb4adb42cea1c
Krunal Bhandekar <krunalbhandekar10@gmail.com>
2022-10-29 08:37:28 +0530

    email validation

---
+++ email validation/index.js
@@ -0,0 +1,9 @@
+// It is recommended to do validations on the server side instead of the client side. Because the javascript can be disabled on the client side.
+
+function validateEmail(email) {
+  var re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+let em = "krunal@gmail.com"
+console.log(validateEmail(em))
